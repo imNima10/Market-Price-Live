fd80f9fd17a6f3ab05ba9e4d470ba865e1525c84
imNima10 <x1nima1x@gmail.com>
2025-11-23 18:53:02 +0330

    Add app and server files configs

---
+++ server.js
@@ -0,0 +1,23 @@
+let app = require("./app")
+let redis = require("./db/redis")
+let { db } = require("./db/mysql");
+
+(async function () {
+    try {
+        await db.authenticate().then(() => {
+            console.log("connect to DB");
+        })
+
+        await redis.ping().then((pong) => {
+            console.log(`PING -> ${pong}`);
+        })
+
+        await app.listen(process.env.PORT, () => {
+            console.log(`server run on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        await mongoose.disconnect()
+        await redis.disconnect()
+        throw error
+    }
+})()
