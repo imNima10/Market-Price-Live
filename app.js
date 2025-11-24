d788b414a22b6aa1e18cacfa60a767cf8bf60178
imNima10 <x1nima1x@gmail.com>
2025-11-24 16:16:41 +0330

    Add 404 error handler and page

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use((req,res)=>{
+    return res.render("404")
+})
+
 module.exports = app
