6de5a341ebeebe4a5df78b3ac448e5dffd05c974
imNima10 <x1nima1x@gmail.com>
2025-11-26 18:40:59 +0330

    Add otpVerify route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
+let buildError = require("../utils/buildError")
 let { createOtp } = require("../utils/otp")
-let { setOtp, setUserKey } = require("../utils/redis")
+let { setOtp, setUserKey, getUserKeyDetails, getOtpDetails, getOtpPattern, getUserKeyPattern, delUserKey, delOtp } = require("../utils/redis")
 let uuidV4 = require("uuid").v4
+let { Users } = require("../db/mysql")
+let bcrypt = require("bcrypt")
 
 exports.getLoginPage = (req, res) => {
     return res.render("login")
@@ -8,7 +11,7 @@ exports.getLoginPage = (req, res) => {
 
 exports.login = async (req, res, next) => {
     try {
-        let {email}=req.body
+        let { email } = req.body
 
         let otp = await createOtp(6)
         let userKey = uuidV4()
@@ -17,7 +20,7 @@ exports.login = async (req, res, next) => {
 
         //TODO (send otp)
 
-        return res.render("otp",{
+        return res.render("otp", {
             userKey
         })
     } catch (error) {
@@ -26,5 +29,41 @@ exports.login = async (req, res, next) => {
             error: "Local Login Error",
             message: error.message
         })
+    }
+}
+
+exports.otpVerify = async (req, res, next) => {
+    try {
+        let { userKey, otp } = req.body
+
+        let email = await getUserKeyDetails(userKey)
+        if (email.expired) {
+            throw buildError({ title: "Invalid link", message: "Invalid or expired login link , please request a new one", status: 401 })
+        }
+        email = email.email
+        let isOtpExists = await getOtpDetails(userKey)
+        if (isOtpExists.expired) {
+            throw buildError({ title: "OTP has expired", message: "OTP has expired, please request a new one", status: 401 })
+        }
+
+        let isOtpValid = await bcrypt.compare(otp, isOtpExists.otp)
+        if (!isOtpValid) {
+            throw buildError({ title: "Incorrect OTP", message: "Incorrect OTP, please try again", status: 401 })
+        }
+
+        let user = await Users.findOne({ where: { email } })
+        if (!user) {
+            let isFirstUser = await Users.count()
+            user = await Users.create({ email, role: isFirstUser == 0 ? "ADMIN" : "USER" })
+        }
+
+        await delUserKey(userKey)
+        await delOtp(userKey)
+
+        // TODO (create and use acccess/refresh tokens)
+
+        return res.render("dashboard")
+    } catch (error) {
+        next(error)
     }
 }
--- routers/auth.js
+++ routers/auth.js
@@ -1,12 +1,14 @@
-let express=require("express")
+let express = require("express")
 
-let router=express.Router()
+let router = express.Router()
 
-let controller=require("./../controllers/auth")
+let controller = require("./../controllers/auth")
 
 router.route("/login")
-.get(controller.getLoginPage)
-.post(controller.login)
+    .get(controller.getLoginPage)
+    .post(controller.login)
 
+router.route("/verify")
+    .post(controller.otpVerify)
 
-module.exports=router
+module.exports = router
