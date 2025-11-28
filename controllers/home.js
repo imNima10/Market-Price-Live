a169ca26c370ea263873c02b92206fa7fdc641c8
imNima10 <x1nima1x@gmail.com>
2025-11-28 17:13:16 +0330

    Add get pages by action route

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,20 @@
-exports.dashboard = async (req, res, next) => {
-    return res.render("dashboard")
+let api = require("../services/api")
+exports.getDashboard = async (req, res, next) => {
+    let assets=[]//TODO
+    
+    return res.render("dashboard", {
+        action: "dashboard",
+        isLogin: false,
+        assets
+    })
+}
+
+exports.getByAction = async (req, res, next) => {
+    let { action } = req.params
+    let assets = await api.get(action)
+    return res.render("dashboard", {
+        action: action,
+        isLogin: true,
+        assets
+    })
 }
--- routers/home.js
+++ routers/home.js
@@ -4,7 +4,11 @@ let router = express.Router()
 
 let controller = require("./../controllers/home")
 
+let { authGuard } = require("./../middlewares/guards")
+
 router.route("/")
-    .get(controller.dashboard)
+    .get(controller.getDashboard)
+router.route("/:action")
+    .get(authGuard, controller.getByAction)
 
 module.exports = router
