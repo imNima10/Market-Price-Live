e329820d1f945560055b33a101c2d306b93e781c
imNima10 <x1nima1x@gmail.com>
2025-12-03 16:58:53 +0330

    Refactor errorHandler middlewares and error view

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,11 +5,33 @@ module.exports = (err, req, res, next) => {
 
     logger(err)
 
-    let status = err.status || 500;
-    return res.status(status).render("error", {
-        status,
-        title: err.title || "خطای سرور",
-        message: err.message || "مشکلی رخ داده لطفا دوباره تلاش کنید"
+    switch (err.status) {
+        case 500:
+            err = {
+                status: err.status,
+                title: "خطای سرور",
+                message: "یک خطای داخلی سرور رخ داده است. لطفاً بعداً دوباره تلاش کنید."
+            }
+            break;
+        case 403:
+            err = {
+                status: err.status,
+                title: "دسترسی غیر مجاز",
+                message: "شما اجازه دسترسی به این بخش را ندارید."
+            }
+            break;
+        case 401:
+            err = {
+                status: err.status,
+                title: "عدم دسترسی",
+                message: "برای دسترسی به این بخش باید وارد حساب کاربری خود شوید."
+            }
+            break;
+    }
+
+    return res.status(err.status).render("error", {
+        status: err.status,
+        title: err.title,
+        message: err.message
     });
 }
-// title: "خطای سرور", message: "یک خطای داخلی سرور رخ داده است. لطفاً بعداً دوباره تلاش کنید.",
