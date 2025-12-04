11ecbe7b108d4c4f9d51dff82a03b9bb43248ff6
imNima10 <x1nima1x@gmail.com>
2025-12-04 18:38:04 +0330

    Add validator package and route validations

--- routers/auth.js
+++ routers/auth.js
@@ -4,13 +4,15 @@ let router = express.Router()
 
 let controller = require("./../controllers/auth")
 const { authGuard } = require("../middlewares/guards")
+let validator = require("./../middlewares/validator")
+const { auth_loginSchema, auth_verifySchema } = require("../validators")
 
 router.route("/login")
     .get(controller.getLoginPage)
-    .post(controller.login)
+    .post(validator({ validate: auth_loginSchema, from: "body", inline: true }), controller.login)
 
 router.route("/verify")
-    .post(controller.otpVerify)
+    .post(validator({ validate: auth_verifySchema, from: "body", inline: true }), controller.otpVerify)
 
 router.route("/logout")
     .post(authGuard(), controller.logout)
